8b910b2a0089f3b47639eaa856da5b29b7c3df37
Aditya Prakarsa <aprakarsa@gmail.com>
2019-01-28 00:42:11 +0800

    prepare for production

--- outsource/outsource/deploy.py
+++ outsource/outsource/deploy.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 import sys
 
-from django.core.wsgi import 
+from django.core.wsgi import
+
+from whitenoise.django import DjangoWhiteNoise 
 
 path = "/home/outsource/outsource/outsource"
 if path not in sys.path:
@@ -19,3 +21,4 @@ if path not in sys.path:
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "outsource.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
